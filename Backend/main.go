aabbd0a71ebf5d11334f5a9a7aa2f45e8b5374f7
Stanley <stanley@goodresearch.com>
2023-09-27 22:43:49 -0700

    iterates over playlist using slices of 100

--- Backend/main.go
+++ Backend/main.go
@@ -5,15 +5,26 @@ import (
 	"fmt"
 )
 
+// calculate how many slices of 100 goes into length
+func calc_slices(length int) int {
+	if length <= 0 {
+		return 0
+	}
+
+	return (length + 99) / 100
+}
+
 func main() {
 	id := "01NTza8NIKI7vBIz1jRJD6"
-	auth_token := "BQDN_qn0pw7XyHUyvSgR9Y1cdrFkMc_tZKme_V5KHEmqALeQBJidIQMxIhiWJDLV08XZFNl_NMmRj_qqEON6FPC5uD01bkJIiD8EF6XqULg8xUE-e5w"
-	resp, err := spotify.Get_playlist(0, id, auth_token)
-	if err != nil {
-		fmt.Println(err.Error())
+	auth_token := "BQAYOSbvnug0qJV_wqQ98UUPc9EUyxkfQkeQMDMDI6fRnKXHNJGt3AUdSqkBEZhyveZ_G2_kASQv9BKxbaN0VDEaJLY54L-shvpQ1KMSRbcnoigi8r0"
+	length := spotify.Get_playlist_length(id, auth_token)
+	slices := calc_slices(length)
+	for i := 0; i < slices; i++ {
+		resp, err := spotify.Get_playlist_children(0, id, auth_token)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println(resp)
 	}
-	fmt.Println(resp)
-	// sorted_lists := openai.Send()
-	// fmt.Println(sorted_lists)
 
 }
--- Backend/spotify/get.go
+++ Backend/spotify/get.go
@@ -1,11 +1,15 @@
 package spotify
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
+type ResponseData struct {
+	Total int `json:"total"`
+}
 type track struct {
 	Spotify_id string `json:"spotify_id"`
 	Name       string `json:"name"`
@@ -17,10 +21,44 @@ type playlist struct {
 	Items      []track `json:"items"`
 }
 
-func Get_playlist(index int, playlistID string, authToken string) (string, error) {
+func Get_playlist_length(playlistID string, authToken string) int {
+	url := fmt.Sprintf("https://api.spotify.com/v1/playlists/%s/tracks?offset=0&limit=100&fields=items(track(name,id,artists(name)))?limit=100", playlistID)
+	// Send an HTTP GET request to the Spotify playlist URL
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+
+	// Set the Authorization header with the access token
+	req.Header.Set("Authorization", "Bearer "+authToken)
+
+	// Send the request and get the response
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return 0
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return 0
+	}
+	// Parse the JSON data into a struct
+	var responseData ResponseData
+	if err := json.Unmarshal(body, &responseData); err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		return 0
+	}
+
+	// Return the value of the "total" key
+	return responseData.Total
+
+}
+
+func Get_playlist_children(index int, playlistID string, authToken string) (string, error) {
 	// goes in limits of 100 songs
 
-	//url := fmt.Sprintf("https://api.spotify.com/v1/playlists/%s/tracks?offset=100&limit=100fields=items(track(name,id,artists(name)))?limit=100", playlistID)
 	url := fmt.Sprintf("https://api.spotify.com/v1/playlists/%s/tracks?offset=%d&limit=100&fields=items(track(name,id,artists(name)))?limit=100", playlistID, index)
 	// Send an HTTP GET request to the Spotify playlist URL
 	client := &http.Client{}
