6163b1bd5556d3e04eee36808c29a6a4a1febed0
Stanley <stanley@goodresearch.com>
2023-09-27 23:25:35 -0700

    Adds all songs from playlist into playlistData struct

--- Backend/main.go
+++ Backend/main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"backend/spotify"
+	"encoding/json"
 	"fmt"
 )
 
+type Artist struct {
+	Name string `json:"name"`
+}
+
+type Track struct {
+	SpotifyID string   `json:"id"`
+	Name      string   `json:"name"`
+	Artists   []Artist `json:"artists"`
+}
+
+type PlaylistItem struct {
+	Track Track `json:"track"`
+}
+
+var playlistData struct {
+	Items []PlaylistItem `json:"items"`
+}
+
 // calculate how many slices of 100 goes into length
-func calc_slices(length int) int {
+func calcSlices(length int) int {
 	if length <= 0 {
 		return 0
 	}
@@ -16,15 +35,29 @@ func calc_slices(length int) int {
 
 func main() {
 	id := "01NTza8NIKI7vBIz1jRJD6"
-	auth_token := "BQAYOSbvnug0qJV_wqQ98UUPc9EUyxkfQkeQMDMDI6fRnKXHNJGt3AUdSqkBEZhyveZ_G2_kASQv9BKxbaN0VDEaJLY54L-shvpQ1KMSRbcnoigi8r0"
-	length := spotify.Get_playlist_length(id, auth_token)
-	slices := calc_slices(length)
+	authToken := "BQAYOSbvnug0qJV_wqQ98UUPc9EUyxkfQkeQMDMDI6fRnKXHNJGt3AUdSqkBEZhyveZ_G2_kASQv9BKxbaN0VDEaJLY54L-shvpQ1KMSRbcnoigi8r0"
+	length := spotify.Get_playlist_length(id, authToken)
+	slices := calcSlices(length)
+
 	for i := 0; i < slices; i++ {
-		resp, err := spotify.Get_playlist_children(0, id, auth_token)
+		resp, err := spotify.Get_playlist_children(i*100, id, authToken)
 		if err != nil {
 			fmt.Println(err.Error())
 		}
-		fmt.Println(resp)
+
+		var playlistItems struct {
+			Items []PlaylistItem `json:"items"`
+		}
+
+		if err := json.Unmarshal([]byte(resp), &playlistItems); err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		playlistData.Items = append(playlistData.Items, playlistItems.Items...)
+
 	}
 
+	fmt.Println("Total items in playlistData:", len(playlistData.Items))
+	fmt.Println(playlistData.Items[950])
 }
--- Backend/spotify/get.go
+++ Backend/spotify/get.go
@@ -10,16 +10,6 @@ import (
 type ResponseData struct {
 	Total int `json:"total"`
 }
-type track struct {
-	Spotify_id string `json:"spotify_id"`
-	Name       string `json:"name"`
-}
-
-type playlist struct {
-	Spotify_id string  `json:"spotify_id"`
-	Name       string  `json:"name"`
-	Items      []track `json:"items"`
-}
 
 func Get_playlist_length(playlistID string, authToken string) int {
 	url := fmt.Sprintf("https://api.spotify.com/v1/playlists/%s/tracks?offset=0&limit=100&fields=items(track(name,id,artists(name)))?limit=100", playlistID)
@@ -59,7 +49,7 @@ func Get_playlist_length(playlistID string, authToken string) int {
 func Get_playlist_children(index int, playlistID string, authToken string) (string, error) {
 	// goes in limits of 100 songs
 
-	url := fmt.Sprintf("https://api.spotify.com/v1/playlists/%s/tracks?offset=%d&limit=100&fields=items(track(name,id,artists(name)))?limit=100", playlistID, index)
+	url := fmt.Sprintf("https://api.spotify.com/v1/playlists/%s/tracks?offset=%d&limit=100&fields=items(track(name,id,artists(name)))", playlistID, index)
 	// Send an HTTP GET request to the Spotify playlist URL
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
