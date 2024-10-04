9e88dbb4a124851a0ce4bb4ca9bc9b9d771ffdd1
Stanley Good <stanleygvi@gmail.com>
2024-10-03 23:56:46 -0700

    ran black on .py files

--- Backend/app.py
+++ Backend/app.py
@@ -9,7 +9,7 @@ from Backend.spotify_api import (
     refresh_access_token,
     get_all_playlists,
     exchange_code_for_token,
-    get_user_id
+    get_user_id,
 )
 from Backend.playlist_processing import process_playlists
 from Backend.helpers import generate_random_string
@@ -17,17 +17,17 @@ from Backend.helpers import generate_random_string
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY")
-app.config['SESSION_TYPE'] = 'redis'
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_USE_SIGNER'] = True
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=1)
+app.config["SESSION_TYPE"] = "redis"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_USE_SIGNER"] = True
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(hours=1)
 
 redis_url = os.getenv("REDIS_URL")
 app.config["SESSION_REDIS"] = redis.from_url(redis_url)
 
-app.config['SESSION_COOKIE_DOMAIN'] = '.splitifytool.com'
-app.config['SESSION_COOKIE_SECURE'] = True
-app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+app.config["SESSION_COOKIE_DOMAIN"] = ".splitifytool.com"
+app.config["SESSION_COOKIE_SECURE"] = True
+app.config["SESSION_COOKIE_SAMESITE"] = "None"
 
 redis_url = os.getenv("REDIS_URL")
 sess = Session()
@@ -37,6 +37,7 @@ CORS(app, origins=["https://splitifytool.com"], supports_credentials=True)
 
 redis_url = os.getenv("REDIS_URL")
 db = redis.from_url(redis_url)
+
 
 @app.route("/login")
 def login_handler():
@@ -50,63 +51,60 @@ def login_handler():
                 db.set(f"{uid}_TOKEN", new_auth_token)
             else:
                 return redirect_to_spotify_login()
-        
+
         response = make_response(redirect("https://splitifytool.com/input-playlist"))
         response.set_cookie(
-            "auth_token", 
-            auth_token, 
-            httponly=True,
-            secure=True,
-            samesite='None'
+            "auth_token", auth_token, httponly=True, secure=True, samesite="None"
         )
         return response
     return redirect_to_spotify_login()
+
 
 def redirect_to_spotify_login():
     client_id = os.getenv("CLIENT_ID")
     state = generate_random_string(16)
     scope = "user-read-private user-read-email playlist-modify-public playlist-modify-private playlist-read-collaborative"
-    
+
     params = {
         "response_type": "code",
         "client_id": client_id,
         "scope": scope,
         "show_dialog": "true",
-        "redirect_uri": url_for('callback_handler', _external=True),
-        "state": state
+        "redirect_uri": url_for("callback_handler", _external=True),
+        "state": state,
     }
-    
-    url = "https://accounts.spotify.com/authorize?" + "&".join([f"{key}={value}" for key, value in params.items()])
+
+    url = "https://accounts.spotify.com/authorize?" + "&".join(
+        [f"{key}={value}" for key, value in params.items()]
+    )
     return redirect(url)
+
 
 @app.route("/callback")
 def callback_handler():
     code = request.args.get("code")
-    
+
     if not code:
         return "No code present in callback", 400
 
     token_data = exchange_code_for_token(code)
-    
+
     if not token_data:
         return "Error exchanging code for token", 500
 
     auth_token = token_data.get("access_token")
     user_id = get_user_id(auth_token)
-    
+
     db.set(f"{user_id}_TOKEN", auth_token)
     db.set(f"{user_id}_REFRESH_TOKEN", token_data.get("refresh_token"))
 
     response = make_response(redirect("https://splitifytool.com/input-playlist"))
     response.set_cookie(
-        "auth_token", 
-        auth_token, 
-        httponly=True,
-        secure=True,
-        samesite='None'
+        "auth_token", auth_token, httponly=True, secure=True, samesite="None"
     )
 
     return response
+
 
 @app.route("/user-playlists")
 def get_playlist_handler():
@@ -116,11 +114,12 @@ def get_playlist_handler():
         return {"Code": 401, "Error": "Authorization token required"}, 401
 
     playlists = get_all_playlists(auth_token)
-    
+
     if not playlists:
         return {"Code": 500, "Error": "Failed to get playlists"}
 
     return jsonify(playlists)
+
 
 @app.route("/process-playlist", methods=["POST"])
 def process_playlist_handler():
@@ -132,12 +131,13 @@ def process_playlist_handler():
 
     assert request.json
     playlist_ids = request.json.get("playlistIds", [])
-    
+
     if not playlist_ids:
         return "No playlist IDs provided", 400
     process_playlists(auth_token, playlist_ids)
 
     return jsonify({"message": "Playlists processed successfully!"}), 200
+
 
 if __name__ == "__main__":
     port = os.getenv("PORT", "8080")
--- Backend/playlist_processing.py
+++ Backend/playlist_processing.py
@@ -1,8 +1,17 @@
 from threading import Thread
-from Backend.spotify_api import get_playlist_length, get_playlist_children, create_playlist, add_songs, get_user_id, get_audio_features, get_playlist_name
+from Backend.spotify_api import (
+    get_playlist_length,
+    get_playlist_children,
+    create_playlist,
+    add_songs,
+    get_user_id,
+    get_audio_features,
+    get_playlist_name,
+)
 from Backend.helpers import calc_slices
 from Backend.grouping import cluster_df
 import time
+
 
 def extract_ids(playlist_data):
     track_ids = []
@@ -11,7 +20,10 @@ def extract_ids(playlist_data):
             track_ids.append(track["track"]["id"])
     return track_ids
 
-def clean_audio_features(audio_features:list[dict[str,float] ],remove_keys:list[str]):
+
+def clean_audio_features(
+    audio_features: list[dict[str, float]], remove_keys: list[str]
+):
     index = 0
     index_remove = []
     for feature in audio_features:
@@ -20,10 +32,9 @@ def clean_audio_features(audio_features:list[dict[str,float] ],remove_keys:list[
                 feature.pop(key, None)
         else:
             index_remove.append(index)
-        index+=1
+        index += 1
     for i in index_remove:
         audio_features.pop(i)
-
 
 
 def process_playlists(auth_token, playlist_ids):
@@ -34,17 +45,20 @@ def process_playlists(auth_token, playlist_ids):
             print(f"Error fetching playlist length for {playlist_id}")
             continue
 
-        thread = Thread(target=process_single_playlist, args=(auth_token, playlist_id, length))
+        thread = Thread(
+            target=process_single_playlist, args=(auth_token, playlist_id, length)
+        )
         thread.start()
         threads.append(thread)
 
     for thread in threads:
         thread.join()
 
+
 def process_single_playlist(auth_token, playlist_id, total_length):
     name = get_playlist_name(playlist_id, auth_token)
     slices = calc_slices(total_length)
-    playlist_data_store = {"id":playlist_id,"tracks": []}
+    playlist_data_store = {"id": playlist_id, "tracks": []}
 
     for i in range(0, slices * 100, 100):
         append_to_playlist_data(i, playlist_id, auth_token, playlist_data_store)
@@ -56,42 +70,56 @@ def process_single_playlist(auth_token, playlist_id, total_length):
     num_playlists = len(grouped["cluster"].value_counts())
 
     threads = []
-    for num in range(0,num_playlists):
+    for num in range(0, num_playlists):
         cluster = grouped[grouped["cluster"] == num]
-        thread = Thread(target=created_and_populate, args=(cluster, user_id, auth_token, name))
+        thread = Thread(
+            target=created_and_populate, args=(cluster, user_id, auth_token, name)
+        )
         thread.start()
         threads.append(thread)
     for thread in threads:
         thread.join()
+
 
 def created_and_populate(cluster_df, user_id, auth_token, name):
 
     slices = calc_slices(len(cluster_df))
     if slices < 1:
         return
-    
-    playlist_id = create_playlist(user_id, auth_token, f"Split playlist from {name} ", "Made using Splitify: https://splitifytool.com/")
+
+    playlist_id = create_playlist(
+        user_id,
+        auth_token,
+        f"Split playlist from {name} ",
+        "Made using Splitify: https://splitifytool.com/",
+    )
     for position in range(0, slices * 100, 100):
         if (position + 100) > len(cluster_df):
             cluster_slice = cluster_df.iloc[position:]
         else:
-            cluster_slice = cluster_df.iloc[position:position+100]
+            cluster_slice = cluster_df.iloc[position : position + 100]
         track_uris = cluster_slice["uri"].tolist()
-        
+
         status = add_songs(playlist_id, track_uris, auth_token, position)
         time.sleep(0.5)
-        if not status or status.get("Error",None):
-            print(f"Append Error: Playlist{name} split, status {status} starting from index: {position}")
+        if not status or status.get("Error", None):
+            print(
+                f"Append Error: Playlist{name} split, status {status} starting from index: {position}"
+            )
+
 
 def append_to_playlist_data(start_index, playlist_id, auth_token, data_store):
     response = get_playlist_children(start_index, playlist_id, auth_token)
     if response and "items" in response:
-        
+
         track_ids = extract_ids(response["items"])
         audio_features = get_audio_features(track_ids, auth_token)
-        clean_audio_features(audio_features, ["type", "id", "track_href", "analysis_url", "duration_ms"])
+        clean_audio_features(
+            audio_features, ["type", "id", "track_href", "analysis_url", "duration_ms"]
+        )
         data_store["tracks"].extend(audio_features)
-        print(f"Appended {len(response["items"])} tracks from playlist starting at index {start_index}")
+        print(
+            f"Appended {len(response["items"])} tracks from playlist starting at index {start_index}"
+        )
     else:
         print(f"Failed to append playlist data from index {start_index}")
-
